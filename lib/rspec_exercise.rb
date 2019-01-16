492de2a5125555c77222bd602ebe33c23e5402d6
romebell <romebell@me.com>
2019-01-16 15:33:42 -0800

    Add #two_sum and specs

--- lib/rspec_exercise.rb
+++ lib/rspec_exercise.rb
@@ -8,4 +8,16 @@ class Array
 
     results
   end
+
+  def two_sum
+    results = []
+
+    self.length.times do |i|
+      ((i + 1)...self.length).each do |j|
+        results << [i,j] if self[i] + self[j] == 0
+      end
+    end
+
+    results
+  end
 end
