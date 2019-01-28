a1f0ea72805d82c86969cd32a663fe9df0649858
romebell <romebell@me.com>
2019-01-28 15:42:57 -0800

    Add #show #update #destroy #create and private method #user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,55 @@
+require 'byebug'
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the index action!"
+    user = User.all
+    render json: user
   end
 
   def create
-    render json: params
+    user = User.new(user_params)
+    debugger
+    if user.save!
+      user.save!
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
-    render json: params
+    user = User.find(params[:id])
+    
+    if user
+      render json: user
+    else 
+      render json: user.errors.full_messages
+    end
   end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+
+    if user
+      user.delete
+      render json: user
+    else
+      render json: user.errors.full_messages
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
