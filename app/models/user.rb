34759a5a6b93b7321de2884e3e17a5cbd6872148
romebell <romebell@me.com>
2019-01-28 16:52:07 -0800

    Add Gemfiles and update schema and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
-class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  username   :string
+#
 
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+
+  has_many :artworks,
+    primary_key: :id,
+    foreign_key: :artist_id,
+    class_name: 'Artwork'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_28_225608) do
+ActiveRecord::Schema.define(version: 2019_01_29_004144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
+  create_table "artworks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "image_url", null: false
+    t.integer "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["artist_id", "title"], name: "index_artworks_on_artist_id_and_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
