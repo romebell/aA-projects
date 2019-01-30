6921d73e0089407c80ee99b58c622eadbbadcdcd
romebell <romebell@me.com>
2019-01-30 15:15:06 -0800

    Add routes and controllers for users and sessions and form for session/new

--- ninetynine_cats_2/app/models/user.rb
+++ ninetynine_cats_2/app/models/user.rb
@@ -1,2 +1,40 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  attr_reader :password 
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentionals(username, password)
+    user = User.find_by(username: username)
+    if user 
+     return user if self.is_password?(password)
+     nil
+    else
+      return nil
+    end
+  end
+
 end
