2aafa21b95343576628e7b84ba0fdee91e485c47
Rome Bell <romebell@me.com>
2019-01-31 14:25:50 -0800

    Add #password=(password) #is_password? User::find_by_credentials

--- Week4/music_app_w4d4/app/models/user.rb
+++ Week4/music_app_w4d4/app/models/user.rb
@@ -14,9 +14,25 @@ class User < ApplicationRecord
     validates :email, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true
     validates :password, length: { minimum: 8, allow_nil: true }
-
+    after_initialize :ensure_session_token
+    
     def self.generate_session_token
-        self.session_token = SecureRandom::urlsafe_base64 
+        SecureRandom::urlsafe_base64 
+    end
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user 
+        else
+            nil
+        end
+    end
+
+    attr_reader :password
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
     end
 
     def reset_session_token!
@@ -25,9 +41,13 @@ class User < ApplicationRecord
         self.session_token
     end
 
-    def ensure_session_token
-        self.session_token || User.generate_session_token
+    def password=(password)
+        @password = password 
+        password_digest = BCrypt::Password.create(password)
     end
 
-    
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
 end
