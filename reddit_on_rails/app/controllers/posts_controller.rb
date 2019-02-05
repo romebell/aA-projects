88fac5234d8b684bd1491b54fcb3b866b81af8c7
VivDeG <vivienedeg@gmail.com>
2019-02-05 15:25:59 -0800

    Update posts and users controllers

--- reddit_on_rails/app/controllers/posts_controller.rb
+++ reddit_on_rails/app/controllers/posts_controller.rb
@@ -2,22 +2,46 @@ class PostsController < ApplicationController
   before_action :require_login
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    if @post.save
+      redirect_to sub_url(@post.sub_id)
+    else
+      flash[:errors] = @post.errors.full_messages
+      render :new
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to post_url(@post)
+    else
+      flash[:errors] = @post.errors.full_messages
+      redirect_to post_url(@post)
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy 
+    redirect_to sub_url(@post.sub_id)
   end
-  
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :url, :content, sub_ids: [])
+  end
 end
--- reddit_on_rails/app/controllers/users_controller.rb
+++ reddit_on_rails/app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       login(@user)
       redirect_to subs_url
     else
-      flash[:errors] = @user.errors.full_messages
+      flash.now[:errors] = @user.errors.full_messages
       render :new
     end
   end
