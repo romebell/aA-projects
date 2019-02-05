72c186270e67a44cb6068719546a760aa3201021
VivDeG <vivienedeg@gmail.com>
2019-02-05 12:28:25 -0800

    Add methods to application controller and routes

--- reddit_on_rails/config/routes.rb
+++ reddit_on_rails/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+  resources :subs, except: [:destroy] do
+    resources :posts, except: [:index]
+  end      
 end
