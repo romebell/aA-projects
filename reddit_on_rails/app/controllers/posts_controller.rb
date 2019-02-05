24c7dc3e0c6b778528ee74234a0e0c2202b439f2
VivDeG <vivienedeg@gmail.com>
2019-02-05 12:27:27 -0800

    Create post model controller and table

---
+++ reddit_on_rails/app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  before_action :require_login
+
+  def new
+
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+end
