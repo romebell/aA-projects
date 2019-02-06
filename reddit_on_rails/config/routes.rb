12d0923b5ba8b2ae30cb4dd0c9a4f5fef8e5a302
VivDeG <vivienedeg@gmail.com>
2019-02-05 17:05:57 -0800

    Add views for subs

--- reddit_on_rails/config/routes.rb
+++ reddit_on_rails/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
   resources :subs, except: [:destroy] do
-    resources :posts, except: [:index]
-  end      
+    resources :posts, except: [:index] do
+      resources :comments, only: [:new]
+    end
+  end
+  resources :comments, only: [:create]
 end
